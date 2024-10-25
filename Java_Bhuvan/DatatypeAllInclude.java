09f060057b557af58df8176327b85be67bd0403f
bhuvan <69458259+bhuvanbhoge@users.noreply.github.com>
2024-10-25 16:48:24 +0530

    Update DatatypeAllInclude.java

--- Java_Bhuvan/DatatypeAllInclude.java
+++ Java_Bhuvan/DatatypeAllInclude.java
@@ -1,19 +1,40 @@
 package bhuvan;
 
+/**
+ * This program demonstrates the use of different data types in Java
+ * and prints their values.
+ */
 public class DatatypeAllInclude {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		short i=1;
-		byte j = 1;
-		int a=2;
-		char b='a';
-		float c = 12.4f;
-		double d = 10.11;
-		boolean e = true;
-		
-		System.out.print("Short " + i + "\n"+ "byte " + j+"\n"+ "int "+ a+"\n" +"char "+ b+"\n"+"float "+ c +"\n"+ "double "+ d+"\n"+ "boolean "+ e);
+    public static void main(String[] args) {
+        // Declare variables of various data types
+        short shortVal = 1;
+        byte byteVal = 1;
+        int intVal = 2;
+        char charVal = 'a';
+        float floatVal = 12.4f;
+        double doubleVal = 10.11;
+        boolean booleanVal = true;
 
-	}
-
+        // Print the values of each variable
+        System.out.println("Short: " + shortVal);
+        System.out.println("Byte: " + byteVal);
+        System.out.println("Int: " + intVal);
+        System.out.println("Char: " + charVal);
+        System.out.println("Float: " + floatVal);
+        System.out.println("Double: " + doubleVal);
+        System.out.println("Boolean: " + booleanVal);
+    }
 }
+
+/*
+Output:
+Short: 1
+Byte: 1
+Int: 2
+Char: a
+Float: 12.4
+Double: 10.11
+Boolean: true
+*/
+
