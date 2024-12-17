1e1c8f2f0cefeeb56aaff51db7a8c1fcbde4304e
bhuvan <69458259+bhuvanbhoge@users.noreply.github.com>
2024-12-17 18:10:52 +0530

    .

---
+++ Java_Bhuvan/Try.java
@@ -0,0 +1,10 @@
+package bhuvan;
+
+public class Try {
+
+	public static void main(String[] args) {
+		Employee jb = new Employee();
+		jb.setEmpId(45);
+	}
+
+}
---
+++ Java_Bhuvan/UnboxingExample.java
@@ -0,0 +1,20 @@
+//2. Write a java program for  Unboxing.
+package bhuvan;
+
+public class UnboxingExample {
+	public static void main(String args[]) {
+		//unboxing
+	    Integer obj=new Integer(8);
+	    int a=obj;
+	    System.out.println("implicitly conversion: " + a);//implicitly conversion
+	    int b=obj.intValue();// explicitly conversion
+	    System.out.println("explicitly conversion: " + b);
+	}
+}
+
+/*
+Expected output: 
+implicitly conversion: 8
+explicitly conversion: 8
+
+*/
