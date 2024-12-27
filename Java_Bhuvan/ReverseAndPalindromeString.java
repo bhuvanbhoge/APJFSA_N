8b28b4e8bc509356e8156565d4437a94cf904c79
bhuvan <69458259+bhuvanbhoge@users.noreply.github.com>
2024-12-27 12:21:55 +0530

    Update ReverseAndPalindromeString.java

--- Java_Bhuvan/ReverseAndPalindromeString.java
+++ Java_Bhuvan/ReverseAndPalindromeString.java
@@ -1,38 +1,62 @@
 package bhuvan;
+
 import java.util.*;
+
 public class ReverseAndPalindromeString {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        // Create a scanner to read input from the user
         Scanner sc = new Scanner(System.in);
+
+        // Read the input string
+        System.out.print("Enter a string: ");
         String str = sc.nextLine();
-        int right = str.length();
-        String rev = "";
-        char ch;
-        boolean isPalindrome = true;
-        
-        for(int i=0;i<right/2;i++){
-            if(str.charAt(i) != str.charAt(right-1-i)){
-                isPalindrome = false;
+
+        // Initialize variables
+        int right = str.length(); // Length of the string
+        String rev = "";          // String to store the reversed string
+        char ch;                  // Temporary variable to hold each character
+        boolean isPalindrome = true; // Flag to check if string is a palindrome
+
+        // Check if the string is a palindrome by comparing characters from both ends
+        for (int i = 0; i < right / 2; i++) {
+            if (str.charAt(i) != str.charAt(right - 1 - i)) {
+                isPalindrome = false;  // If characters don't match, it's not a palindrome
+                break;  // No need to continue if already determined it's not a palindrome
             }
         }
-        
-        if(isPalindrome){
+
+        // Output whether the string is a palindrome or not
+        if (isPalindrome) {
             System.out.println("Palindrome");
-        }else{
-            System.out.println("Non palindrome");
+        } else {
+            System.out.println("Non-palindrome");
         }
-        
-        
-        for(int i=0;i<right;i++){
-            ch = str.charAt(i);
-            rev = ch+ rev;
+
+        // Reverse the string manually
+        for (int i = 0; i < right; i++) {
+            ch = str.charAt(i); // Get character at index i
+            rev = ch + rev;      // Add it to the beginning of 'rev' to reverse the string
         }
-        if(rev==str){
-            System.out.println("Palindrome");   
-        }else{
-            System.out.println("non palindrome");
+
+        // Check if the reversed string is the same as the original string
+        if (rev.equals(str)) {  // Use equals() to compare the content of the strings
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Non-palindrome");
         }
-        
-        
-        System.out.println("reversed string is " + rev);
+
+        // Output the reversed string
+        System.out.println("Reversed string is: " + rev);
+
+        // Close the scanner to avoid resource leaks
+        sc.close();
     }
 }
+
+/*
+output
+	Enter a string: mam
+Palindrome
+Palindrome
+Reversed string is: mam
+	*/
