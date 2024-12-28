d87618cba3ceaffd53a2cbf136eebb48d09e8f28
bhuvan <69458259+bhuvanbhoge@users.noreply.github.com>
2024-12-28 20:41:37 +0530

    Factorial Without Recursion

---
+++ Java_Bhuvan/FactorialWithoutRecursion.java
@@ -0,0 +1,14 @@
+package bhuvan;
+
+public class FactorialWithoutRecursion {
+	public static void main(String args[]) {
+		int n=5;
+		int fact=1;
+		
+		for(int i=n;i>1;i--) {
+			fact *= i;
+		}
+		System.out.print("Factorial of " + n + " is "+ fact);
+		
+	}
+}
