b5168b967690348e9c4b0971303021fa3d2379cc
bhuvan <69458259+bhuvanbhoge@users.noreply.github.com>
2025-03-30 00:05:36 +0530

    Find Occurrence Of Element Using Binary Search

---
+++ Java_Bhuvan/FindOccuranceOfElementUsingBinarySearch.java
@@ -0,0 +1,64 @@
+package bhuvan;
+
+public class FindOccuranceOfElementUsingBinarySearch {
+	public static void main(String[] args) {
+        //Try to solve it using Binary search
+        int nums[]={5,7,7,7,7,8,8,10};
+        int target=7;
+        
+        int ans[] = searchRange(nums,target);
+        if(ans[0]==-1) {
+        	System.out.println("No Occurance");
+        	return;
+        }
+        int occurance = ans[1]-ans[0]+1;
+        System.out.println("Occurance of " + target + " in given array is "+occurance);
+    }
+    public static int[] searchRange(int[] nums, int target) {
+        int ans[] = {-1,-1};
+        ans[0]=firstOcc(nums,target);
+        if(ans[0]==-1){
+            return ans;
+        }
+        ans[1]=lastOcc(nums,target);
+        return ans;
+    }
+
+    public static int firstOcc(int nums[],int target){
+        int first = -1;
+        int start = 0;
+        int end = nums.length-1;
+
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(nums[mid]==target){
+                first = mid;
+                end=mid-1;
+            }else if(nums[mid]<target){
+                start= mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return first;
+    }
+
+    public static int lastOcc(int nums[],int target){
+        int last = -1;
+        int start = 0;
+        int end=nums.length-1;
+
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                last = mid;
+                start=mid+1;
+            }else if(nums[mid]<target){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return last;
+    }
+}
